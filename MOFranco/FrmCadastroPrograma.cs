4d5239e5474e48be024064bd85a020b572089f3e
jonasbfranco <jonasbfranco@hotmail.com>
2026-04-05 06:22:51 -0300

    Criando Form de Cadastro de Programa de Aquecimento

---
+++ MOFranco/FrmCadastroPrograma.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MOFranco
+{
+    public partial class FrmCadastroPrograma : Form
+    {
+        public FrmCadastroPrograma()
+        {
+            InitializeComponent();
+        }
+    }
+}
